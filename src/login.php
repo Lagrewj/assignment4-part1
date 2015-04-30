932e3b24b9d3df3565176eff9d48d07c22cb8867
Jon Lagrew <lagrewj@onid.oregonstate.edu>
2015-04-30 14:21:49 -0700

    Update login.php

--- src/login.php
+++ src/login.php
@@ -4,8 +4,15 @@ session_start();
 
 <html>
   <head>
-    <body>
-      <form> action="content1.php method="POST">
-    </body>
+    <meta charset="utf-8">
+    <title>Welcome to CS290 Assignment 4</title>
   </head>
+  <body>
+      <form> action="content1.php method="POST">
+      <label for="username">Username</label>
+      <input type="text" name="username" id="username" /><br/>
+      <input type="submit" value="Login"/>
+      </form>
+  </body>
+  
 </html>
