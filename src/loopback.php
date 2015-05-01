07ddf29b028585af9dc55ed076e0b7ff5811afc3
Jon Lagrew <lagrewj@onid.oregonstate.edu>
2015-05-01 15:48:52 -0700

    Update loopback.php

--- src/loopback.php
+++ src/loopback.php
@@ -1,1 +1,43 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Assignment 4 CS290</title>
+</head>
+<body>
 
+<?php
+class JSONClass{
+	public $type;
+	public $parameters = array();
+}
+	$json = new JSONClass(); //creating instance of JSONClass
+	//if POST input
+	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$json->type = "POST";
+		if(empty($_REQUEST)) { //if no parameters, then JSON object key to null
+		$json->parameters = "null";
+		} else {
+			foreach($_REQUEST as $key => $value) { //adding key and value pair to the array
+				$json->parameters[$key] = $value;
+			}
+		}
+	//if GET input
+	} else {
+		$json->type = "GET";
+		if(empty($_REQUEST) == 0) {
+		$json->parameters = "null";
+		} else {
+			foreach($_REQUEST as $key => $value) { //adding key and value pair to the array
+				$json->parameters[$key] = $value;
+			}
+		}
+	}
+	//returns the JSON representation of a value
+	echo json_encode($json); //output json to the user
+//references
+//1. http://php.net/manual/en/function.json-encode.php
+?>
+
+</table>
+</body>
+</html>
