d3becc3e99835c2ca6e77ac600f8b10d64a9dd81
Jon Lagrew <lagrewj@onid.oregonstate.edu>
2015-05-01 19:16:58 -0700

    Update loopback.php

--- src/loopback.php
+++ src/loopback.php
@@ -1,33 +1,25 @@
 <?php
 error_reporting(E_All);
 ini_set('display_errors', 1);
-	$keyValueArray = array();
-	foreach($_POST as $key => $value){
+	$keyValueArray = array(); 
+	foreach($_POST as $key => $value){ //adding key and value pair to the array
 		$keyValueArray[$key] = $value;
 	}
 	//if POST input
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$json->type = "POST";
-		if(empty($_REQUEST)) { //if no parameters, then JSON object key to null
-		$json->parameters = "null";
-		} else {
-			foreach($_REQUEST as $key => $value) { //adding key and value pair to the array
-				$json->parameters[$key] = $value;
-			}
-		}
+		$arrayPost = array("Type" => "POST", "parameters" => $keyValueArray);
+		echo json_encode($arrayPost); //returns the JSON representation of a value
+	}
+	foreach($_GET as $key => $value){ //adding key and value pair to the array
+		$keyValueArray[$key] = $value;
+	}
 	//if GET input
-	} else {
-		$json->type = "GET";
-		if(empty($_REQUEST) == 0) {
-		$json->parameters = "null";
-		} else {
-			foreach($_REQUEST as $key => $value) { //adding key and value pair to the array
-				$json->parameters[$key] = $value;
-			}
-		}
+	if($_SERVER['REQUEST_METHOD'] == 'GET') {
+		$arrayGet = array("Type" => "GET", "parameters" => $keyValueArray);
+		echo json_encode($arrayGet); //returns the JSON representation of a value 
 	}
 	//returns the JSON representation of a value
-	echo json_encode($json); //output json to the user
+	
 //references
 //1. http://php.net/manual/en/function.json-encode.php
 ?>
