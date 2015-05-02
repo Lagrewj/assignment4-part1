d705be094fd1669b7c74c57029150fde335697ea
Jon Lagrew <lagrewj@onid.oregonstate.edu>
2015-05-01 17:14:00 -0700

    Update multtable.php

--- src/multtable.php
+++ src/multtable.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>cs290 Assignment 4</title>
+</head>
+<body>
+<?php 
+$valid_input = true; //parameters passed via the URL in a GET request are valid
+//first checking if parameters passed exist
+if (!isset($_GET['min-multiplicand']) || !isset($_GET['max-multiplicand']) || !isset($_GET['min-multiplier']) ||!isset($_GET['max-multiplier']) ){
+      //if parameters don't exist then valid input set to false and error thrown
+	  echo "<h1>Missing a parameter.<h1>";
+      $valid_input = false;
+    }
