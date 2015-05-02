0243d1201eaacd154a48c2946781820f4229e133
Jon Lagrew <lagrewj@onid.oregonstate.edu>
2015-05-01 20:07:58 -0700

    updated comments

--- src/content1.php
+++ src/content1.php
@@ -2,33 +2,33 @@
 //error_reporting(E_All);
 //ini_set('display_errors' 1);
 
-session_start(); // Start session
+session_start(); //start session
 
-if(session_status() == PHP_SESSION_ACTIVE){
-        if(isset($_POST['username']) && trim($_POST['username']) != "")
+if(session_status() == PHP_SESSION_ACTIVE){ //if session is active
+        if(isset($_POST['username']) && trim($_POST['username']) != "") //checking username not blank
         {
             {
-                $_SESSION['username'] = $_POST['username'];
-                $_SESSION['logged_in'] = true; // If this key exists, other pages can use it to test for login state
+                $_SESSION['username'] = $_POST['username'];//post username
+                $_SESSION['logged_in'] = true; //if the key exists then other pages use for login
                 $valid_login = true;
              }
         }
         elseif (trim($_POST['username']) != "") //warn if username is blank
         {
-            $login_str = '<a href="login.php?logout=true">Click here to return to the login screen.</a>';
-            echo "A username must be entered. ".$login_str;
-            $valid_login = false;
+            $login_str = '<a href="login.php?logout=true">Click here to return to the login screen.</a>'; //link to go back
+            echo "A username must be entered. ".$login_str;  //usernmae not entered 
+            $valid_login = false; //setting to false login
         }
         elseif(isset($_SESSION['username']))
         {
-            $valid_login = true;
+            $valid_login = true; //if username valid then login set to true
         }
         else
         {
-            $filepath = explode('/', $_SERVER['PHP_SELF'], -1);
+            $filepath = explode('/', $_SERVER['PHP_SELF'], -1); //split string by a string
             $filepath = implode('/', $filepath);
             $redirect = "http://".$_SERVER['HTTP_HOST'].$filepath;
-            header("Location: {$redirect}/login.php", true);
+            header("Location: {$redirect}/login.php", true); //redirects if timed out
             die();
         }
         if (!isset($_SESSION['visits'])) //setting number of visits
@@ -38,14 +38,15 @@ if(session_status() == PHP_SESSION_ACTIVE){
         //displaying name and inc visits if valid login
         if($valid_login == true)
         {
-            $_SESSION['visits']++;
-            $visits = $_SESSION['visits'];
-            $username = $_SESSION['username'];
+            $_SESSION['visits']++; //inc
+            $visits = $_SESSION['visits']; //visits
+            $username = $_SESSION['username']; //username
             $logout_str = '<a href="login.php?logout=true">Click here to logout</a>';
             echo "Hello $username you have visited this page $visits times before. ".$logout_str;
             echo "<br><br>";
-            $content2 = '<a href="content2.php">Click here to visit content2.php</a>';
+            $content2 = '<a href="content2.php">Click here to visit content2.php</a>'; //link to content 2
             echo $content2;
         }
     }
+    //reference http://php.net/manual/en/function.explode.php
 ?>
