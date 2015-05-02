4033843dff759dd20499acc730b61ff92f504ba0
Jon Lagrew <lagrewj@onid.oregonstate.edu>
2015-05-01 17:20:23 -0700

    Update multtable.php

--- src/multtable.php
+++ src/multtable.php
@@ -8,8 +8,20 @@
 <?php 
 $valid_input = true; //parameters passed via the URL in a GET request are valid
 //first checking if parameters passed exist
-if (!isset($_GET['min-multiplicand']) || !isset($_GET['max-multiplicand']) || !isset($_GET['min-multiplier']) ||!isset($_GET['max-multiplier']) ){
+if (!isset($_GET['min-multiplicand'])){
       //if parameters don't exist then valid input set to false and error thrown
-	  echo "<h1>Missing a parameter.</h1>";
+	  echo "<h1>Missing min-multiplicand.</h1><br>";
       $valid_input = false;
     }
+elseif(!isset($_GET['max-multiplicand'])){
+	echo "<h1>Missing max-multiplicand.</h1><br>";
+	$valid_input = false;
+	}
+elseif(!isset($_GET['min-multiplier'])){
+	echo "<h1>Missing min-multiplier.</h1><br>";
+	$valid_input = false;
+	}
+elseif(!isset($_GET['max-multiplier'])){
+	echo "<h1>Missing max-multiplier.</h1><br>";
+	$valid_input = false;
+	}
